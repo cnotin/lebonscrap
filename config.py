01d7a152123f4c6cf07ea8af45f1799a3e573df3
ClementNotin <clement@notin.org>
2013-03-09 01:48:03 +0100

    Ajout du charset utf8 dans la connexion à la bdd

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import secret
 
-SQLALCHEMY_DATABASE_URI = u'mysql+mysqldb://%s@localhost/lebonscrap?use_unicode=1' % secret.DB_AUTH
-PHOTO_DIR               = u"/home/clem/PycharmProjects/lebonscrap/photos/"
+SQLALCHEMY_DATABASE_URI = u'mysql+mysqldb://%s@localhost/lebonscrap?use_unicode=1&charset=utf8' \
+                          % secret.DB_AUTH
+PHOTO_DIR = u"/home/clem/PycharmProjects/lebonscrap/photos/"
